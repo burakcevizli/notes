3f896b43a2ba42e8402ee84ffc0136defa13b9fb
KrieqqeR <burakcevizli@gmail.com>
2024-10-01 21:21:16 +0300

    JdbxcDetailsManager added.

--- src/main/java/com/example/notes/security/SecurityConfig.java
+++ src/main/java/com/example/notes/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -44,7 +45,7 @@ public class SecurityConfig {
      */
     @Bean
     public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        JdbcUserDetailsManager manager= new JdbcUserDetailsManager();
         if(!manager.userExists("user1")){
             manager.createUser(User.withUsername("user1").password("{noop}password1").roles("USER").build()); // noop yazan yerde parolanın şifrelenmeden saklandıgını gösteriyor...
         }
