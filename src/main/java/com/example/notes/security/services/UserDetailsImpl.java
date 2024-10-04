7e363676ffea1160fe32ab3d58a39d1f97cea76d
KrieqqeR <burakcevizli@gmail.com>
2024-10-04 22:12:47 +0300

    user modal ve user details implemation area dded....

---
+++ src/main/java/com/example/notes/security/services/UserDetailsImpl.java
@@ -0,0 +1,111 @@
+package com.example.notes.security.services;
+
+import com.example.notes.models.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+import java.util.*;
+
+@NoArgsConstructor
+@Data
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private boolean is2faEnabled;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(Long id, String username, String email, String password, boolean is2faEnabled, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.is2faEnabled = is2faEnabled;
+        this.authorities = authorities;
+    }
+
+    public static UserDetailsImpl fromUser(User user) {
+        GrantedAuthority auhority = new SimpleGrantedAuthority(user.getRole().toString());
+
+        return new UserDetailsImpl(
+                user.getUserId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                user.isTwoFactorEnabled(),
+                List.of(auhority)
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isIs2faEnabled() {
+        return is2faEnabled;
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
+
+    public boolean is2faEnabled() {
+        return is2faEnabled;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id, user.id);
+    }
+}
